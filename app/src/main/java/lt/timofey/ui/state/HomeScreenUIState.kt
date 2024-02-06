b8be2ad0df9d6a751dee6fd1c08e13f1b44f0601
Timofey Lyadinsky <tima.led12@gmail.com>
2024-02-06 23:55:51 +0300

    feat: add ui state for home screen

--- app/src/main/java/lt/timofey/ui/state/HomeScreenUIState.kt
+++ app/src/main/java/lt/timofey/ui/state/HomeScreenUIState.kt
@@ -2,16 +2,20 @@ package lt.timofey.ui.state
 
 import lt.timofey.domain.entity.CuratedPhotos
 import lt.timofey.domain.entity.FeaturedCollections
+import lt.timofey.domain.entity.SearchPhotos
 
 sealed class HomeScreenUIState {
+    private var loadingFeaturedCollections: FeaturedCollectionsUIState = FeaturedCollectionsUIState.LOADING
+    private var loadingCuratedPhotos: CuratedPhotosUIState = CuratedPhotosUIState.LOADING
+    private var loadingSearchedPhotos: SearchPhotosUIState = SearchPhotosUIState.WAIT
     private var isSearched: Boolean = false
-
+    private var searchQuery: String = ""
 }
 
 sealed class FeaturedCollectionsUIState {
     data object LOADING : FeaturedCollectionsUIState()
     data class SUCCESS(val featuredCollections: FeaturedCollections) : FeaturedCollectionsUIState()
-    data class ERROR(val message: String) :FeaturedCollectionsUIState()
+    data class ERROR(val message: String) : FeaturedCollectionsUIState()
 }
 
 sealed class CuratedPhotosUIState {
@@ -20,3 +24,9 @@ sealed class CuratedPhotosUIState {
     data class ERROR(val message: String) : CuratedPhotosUIState()
 }
 
+sealed class SearchPhotosUIState {
+    data object WAIT : SearchPhotosUIState()
+    data object LOADING : SearchPhotosUIState()
+    data class SUCCESS(val searchPhotos: SearchPhotos) : SearchPhotosUIState()
+    data class ERROR(val message: String) : SearchPhotosUIState()
+}
