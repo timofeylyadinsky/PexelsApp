474ccbfeea7fcdcf9ebdc1233020824f448db904
Timofey Lyadinsky <tima.led12@gmail.com>
2024-02-07 16:22:36 +0300

    feat: add navigation func

--- app/src/main/java/lt/timofey/ui/navigation/PexelAppNavigation.kt
+++ app/src/main/java/lt/timofey/ui/navigation/PexelAppNavigation.kt
@@ -1,9 +1,16 @@
 package lt.timofey.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun PexelAppNavigation() {
     val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screens.HomeScreen.route ) {
+        composable(route = Screens.HomeScreen.route) {
+
+        }
+    }
 }
