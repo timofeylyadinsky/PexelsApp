cf21a2b7a41d6e8cb1b2a08dcf51a362c654d248
Timofey Lyadinsky <tima.led12@gmail.com>
2024-02-14 21:25:36 +0300

    feat: add get photos and collections use cases

--- app/src/main/java/lt/timofey/domain/usecases/BookmarkCollectionUseCases.kt
+++ app/src/main/java/lt/timofey/domain/usecases/BookmarkCollectionUseCases.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import lt.timofey.data.repository.impl.BookmarkRepositoryImpl
 import lt.timofey.domain.entity.Photos
+import lt.timofey.domain.entity.toPhotos
 import lt.timofey.domain.entity.toPhotosDbo
 import javax.inject.Inject
 
@@ -31,5 +32,23 @@ class deletePhotosFromBookmarkUseCase @Inject constructor(
 ) {
     suspend operator fun invoke(photos: Photos) = withContext(ioDispatcher){
         bookmarkRepositoryImpl.deletePhoto(photos.toPhotosDbo())
+    }
+}
+
+class getPhotoFromDBUseCase @Inject constructor(
+    private val ioDispatcher: CoroutineDispatcher,
+    private val bookmarkRepositoryImpl: BookmarkRepositoryImpl
+) {
+    suspend operator fun invoke(photos: Photos) = withContext(ioDispatcher){
+        bookmarkRepositoryImpl.getSavedPhotoById(photos.id)
+    }
+}
+
+class getBookmarkCollectionsUseCase @Inject constructor(
+    private val ioDispatcher: CoroutineDispatcher,
+    private val bookmarkRepositoryImpl: BookmarkRepositoryImpl
+) {
+    suspend operator fun invoke() = withContext(ioDispatcher){
+        bookmarkRepositoryImpl.getSavedPhotos().map { it.toPhotos() }
     }
 }
