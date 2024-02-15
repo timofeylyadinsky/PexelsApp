90144dfa7b8362e3c3bb6c75b49aaadbcf03a344
Timofey Lyadinsky <tima.led12@gmail.com>
2024-02-15 15:31:24 +0300

    feat: add bookmark view model

--- app/src/main/java/lt/timofey/ui/state/BookmarkScreenUIState.kt
+++ app/src/main/java/lt/timofey/ui/state/BookmarkScreenUIState.kt
@@ -1,5 +1,14 @@
 package lt.timofey.ui.state
 
+import lt.timofey.domain.entity.Photos
+
 data class BookmarkScreenUIState(
-    var loadingPhoto: LoadingPhotoUIState = LoadingPhotoUIState.LOADING,
+    var loadingPhoto: BookmarkUIState = BookmarkUIState.LOADING,
 )
+
+sealed class BookmarkUIState {
+    data object LOADING : BookmarkUIState()
+    data class SUCCESS(val photos: List<Photos>) : BookmarkUIState()
+    data class ERROR(val message: String) : BookmarkUIState()
+    data class EMPTY(val photos: List<Photos>) : BookmarkUIState()
+}
--- app/src/main/java/lt/timofey/ui/viewmodel/BookmarkScreenViewModel.kt
+++ app/src/main/java/lt/timofey/ui/viewmodel/BookmarkScreenViewModel.kt
@@ -1,10 +1,54 @@
 package lt.timofey.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import lt.timofey.domain.usecases.getBookmarkCollectionsUseCase
+import lt.timofey.ui.state.BookmarkScreenUIState
+import lt.timofey.ui.state.BookmarkUIState
 import javax.inject.Inject
 
 @HiltViewModel
 class BookmarkScreenViewModel @Inject constructor(
-//    private val
-) {
+    private val getBookmarkCollectionsUseCase: getBookmarkCollectionsUseCase
+) : ViewModel() {
+    val uiState = MutableStateFlow<BookmarkScreenUIState>(BookmarkScreenUIState())
+
+    init {
+        viewModelScope.launch {
+            getBookmarkCollection()
+        }
+    }
+
+    private suspend fun getBookmarkCollection() {
+        viewModelScope.launch {
+            try {
+                val bookmark = getBookmarkCollectionsUseCase()
+                if (bookmark.isNotEmpty()) {
+                    uiState.update {
+                        uiState.value.copy(
+                            loadingPhoto = BookmarkUIState.SUCCESS(
+                                bookmark
+                            )
+                        )
+                    }
+                } else
+                {
+                    uiState.update {
+                        uiState.value.copy(
+                            loadingPhoto = BookmarkUIState.EMPTY(
+                                bookmark
+                            )
+                        )
+                    }
+                }
+            }
+            catch (e: Exception) {
+                uiState.update { uiState.value.copy(loadingPhoto = BookmarkUIState.ERROR(e.localizedMessage)) }
+            }
+        }
+    }
 }
